fb666bdca569df6c434a3e6771457e01271d2138
sikandar <sikanderafridi.854@gmail.com>
2026-06-27 02:30:49 +0500

    ...

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,22 @@ import {
   registerUser, 
    
 } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 
 const router = Router()
 
 router.route("/register").post(
+        upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
     registerUser
     )
 
