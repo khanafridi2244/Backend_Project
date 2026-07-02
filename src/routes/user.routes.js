970e949dd28b7d2a0703660f3c15626b7c47f2c0
sikandar <sikanderafridi.854@gmail.com>
2026-07-02 19:35:26 +0500

    add resfreshtoken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { 
     loginUser,
     logoutUser,
-    registerUser
+    registerUser,
+     refreshAccessToken
 
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
@@ -24,8 +25,9 @@ router.route("/register").post(
     registerUser
     )
 
-router.route("/login").post(loginUser)
 
+router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
